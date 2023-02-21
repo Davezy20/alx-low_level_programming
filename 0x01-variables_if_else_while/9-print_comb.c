2ebb9293a6e84df50f8b54841df9160c9b602072
Davezy20 <Davidayomikun14@gmail.com>
2023-02-20 22:37:21 -0800

    9 task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,11 +4,12 @@
  * main - this is the main function
  * Return: this is always 0
  */
+
 int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
 		putchar(i + '0');
 		putchar(',');
